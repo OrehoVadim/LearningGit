3819decf45e3c32a6ec4fcbed0137ebfcce77566
Mykhailo Kuksin <stinkuks@gmail.com>
2024-05-09 23:44:17 -0700

    Cone expanded. Added OOP and noOOP examples. 1.1 solved

--- Assets/LearningScripts/Cone.cs
+++ Assets/LearningScripts/Cone.cs
@@ -1,5 +1,49 @@
+using System.Collections.Generic;
+
 namespace LearningScripts
 {
+    public class Task1NoOOP
+    {
+        public float SumConeVolumes(List<float> radiusList, List<float> heightList)
+        {
+            float sum = 0;
+
+            if (radiusList.Count > heightList.Count)
+            {
+                return 0;
+            }
+            
+            for (int i = 0; i < radiusList.Count; i++)
+            {
+                sum += CalculateConeVolume(radiusList[i], heightList[i]);
+            }
+
+            return sum;
+        }
+        
+        private float CalculateConeVolume(float radius, float height)
+        {
+            return 3f / 4f * 3.1415f * (radius * radius) * height;
+        }
+    }
+
+    //----------------------------------------------------------
+    
+    public class Task1_OOP
+    {
+        public float SumConeVolumesOOP(List<Cone> cones)
+        {
+            float sum = 0;
+
+            foreach (var cone in cones)
+            {
+                sum += cone.CalculateConeVolume();
+            }
+
+            return sum;
+        }
+    }
+
     public class Cone
     {
         public float Radius { get; private set; }
@@ -9,6 +53,11 @@ namespace LearningScripts
         {
             Radius = radius;
             Height = height;
+        }
+        
+        public float CalculateConeVolume()
+        {
+            return 3f / 4f * 3.1415f * (Radius * Radius) * Height;
         }
     }
 }
